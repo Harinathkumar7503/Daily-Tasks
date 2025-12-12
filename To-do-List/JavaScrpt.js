012f6fe7343a25ff8ce27f774446a45f3ef5aea7
Harinath kumar <harinath7503@gmail.com>
2025-12-12 12:05:08 +0530

    T0 -do-list
    Rakesk trining task

---
+++ To-do-List/JavaScrpt.js
@@ -0,0 +1,26 @@
+
+function addTask()
+{
+   
+ let text= document.getElementById('text').value;
+
+ const create=document.createElement('li');
+ create.textContent=text;
+
+ const UL=document.querySelector('ul');
+ UL.append(create);
+ 
+ const removeBT= document.createElement('button');
+ removeBT.textContent="REMOVE";
+ removeBT.className="remove-btn";
+
+ removeBT.addEventListener("click", function(Remove)
+{
+
+     create.remove();
+});    
+
+create.append(removeBT);
+
+ 
+}
